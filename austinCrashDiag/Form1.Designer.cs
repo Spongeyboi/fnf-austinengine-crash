f5522ecec253f1481273039c33552e28b72c80f4
Spongeyboi <spongebob7989a@gmail.com>
2022-07-07 15:27:03 -0400

    Add project files.

---
+++ austinCrashDiag/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace austinCrashDiag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.QuitBtn = new System.Windows.Forms.Button();
+            this.SaveStackBtn = new System.Windows.Forms.Button();
+            this.RestartBtn = new System.Windows.Forms.Button();
+            this.DiscordBtn = new System.Windows.Forms.Button();
+            this.SaveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.22422F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.77578F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TextBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.RichTextBox1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.QuitBtn, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.RestartBtn, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.SaveStackBtn, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.DiscordBtn, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.00971F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.99029F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(449, 299);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 3);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Austin Engine Crashed";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = ".\\assets\\images\\ui\\crash-bf.png";
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.pictureBox1, 2);
+            this.pictureBox1.Size = new System.Drawing.Size(109, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 3);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 60);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Unfortunately, Austin Engine has crashed. Below is a crash dump that lists the er" +
+    "ror. Report this on the discord server.";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.BackColor = System.Drawing.Color.White;
+            this.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.TextBox1, 3);
+            this.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBox1.ForeColor = System.Drawing.Color.Black;
+            this.TextBox1.Location = new System.Drawing.Point(118, 139);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(328, 13);
+            this.TextBox1.TabIndex = 3;
+            this.TextBox1.Text = "Error goes here";
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.BackColor = System.Drawing.Color.White;
+            this.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.RichTextBox1, 3);
+            this.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RichTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.RichTextBox1.Location = new System.Drawing.Point(118, 169);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(328, 75);
+            this.RichTextBox1.TabIndex = 4;
+            this.RichTextBox1.Text = "Stack goes here";
+            // 
+            // QuitBtn
+            // 
+            this.QuitBtn.Location = new System.Drawing.Point(340, 275);
+            this.QuitBtn.Name = "QuitBtn";
+            this.QuitBtn.Size = new System.Drawing.Size(75, 21);
+            this.QuitBtn.TabIndex = 7;
+            this.QuitBtn.Text = "Quit";
+            this.QuitBtn.UseVisualStyleBackColor = true;
+            this.QuitBtn.Click += new System.EventHandler(this.QuitBtn_Click);
+            // 
+            // SaveStackBtn
+            // 
+            this.SaveStackBtn.Location = new System.Drawing.Point(118, 275);
+            this.SaveStackBtn.Name = "SaveStackBtn";
+            this.SaveStackBtn.Size = new System.Drawing.Size(75, 21);
+            this.SaveStackBtn.TabIndex = 6;
+            this.SaveStackBtn.Text = "Save Stack";
+            this.SaveStackBtn.UseVisualStyleBackColor = true;
+            this.SaveStackBtn.Click += new System.EventHandler(this.SaveStackBtn_Click);
+            // 
+            // RestartBtn
+            // 
+            this.RestartBtn.Location = new System.Drawing.Point(231, 275);
+            this.RestartBtn.Name = "RestartBtn";
+            this.RestartBtn.Size = new System.Drawing.Size(75, 21);
+            this.RestartBtn.TabIndex = 5;
+            this.RestartBtn.Text = "Restart";
+            this.RestartBtn.UseVisualStyleBackColor = true;
+            this.RestartBtn.Click += new System.EventHandler(this.RestartBtn_Click);
+            // 
+            // DiscordBtn
+            // 
+            this.DiscordBtn.Location = new System.Drawing.Point(3, 275);
+            this.DiscordBtn.Name = "DiscordBtn";
+            this.DiscordBtn.Size = new System.Drawing.Size(75, 21);
+            this.DiscordBtn.TabIndex = 8;
+            this.DiscordBtn.Text = "Discord";
+            this.DiscordBtn.UseVisualStyleBackColor = true;
+            this.DiscordBtn.Click += new System.EventHandler(this.DiscordBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 299);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Austin Engine Crashed";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.RichTextBox RichTextBox1;
+        private System.Windows.Forms.Button QuitBtn;
+        private System.Windows.Forms.Button SaveStackBtn;
+        private System.Windows.Forms.Button RestartBtn;
+        private System.Windows.Forms.Button DiscordBtn;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog1;
+    }
+}
+
---
+++ austinCrashDiag/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace austinCrashDiag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MemoryStream userInput = new MemoryStream();
+
+        private void DiscordBtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/efE9c9AkWy");
+        }
+
+        private void SaveStackBtn_Click(object sender, EventArgs e)
+        {
+            // Set the properties on SaveFileDialog1 so the user is 
+            // prompted to create the file if it doesn't exist 
+            // or overwrite the file if it does exist.
+            SaveFileDialog1.CreatePrompt = true;
+            SaveFileDialog1.OverwritePrompt = true;
+
+            // Set the file name to myText.txt, set the type filter
+            // to text files, and set the initial directory to the 
+            // MyDocuments folder.
+            SaveFileDialog1.FileName = "myText";
+            // DefaultExt is only used when "All files" is selected from 
+            // the filter box and no extension is specified by the user.
+            SaveFileDialog1.DefaultExt = "txt";
+            SaveFileDialog1.Filter =
+                "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            SaveFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Call ShowDialog and check for a return value of DialogResult.OK,
+            // which indicates that the file was saved. 
+            DialogResult result = SaveFileDialog1.ShowDialog();
+            Stream fileStream;
+
+            if (result == DialogResult.OK)
+            {
+                // Open the file, copy the contents of memoryStream to fileStream,
+                // and close fileStream. Set the memoryStream.Position value to 0 
+                // to copy the entire stream. 
+                fileStream = SaveFileDialog1.OpenFile();
+                userInput.Position = 0;
+                userInput.WriteTo(fileStream);
+                fileStream.Close();
+                SaveStackBtn.Text = "Stack Saved";
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string errorText = System.IO.File.ReadAllText(@"logs\latestError.txt");
+                string errorStackText = System.IO.File.ReadAllText(@"logs\latestStack.txt");
+
+                if (errorText != "") TextBox1.Text = errorText;
+                if (errorStackText != "") RichTextBox1.Text = errorStackText;
+            }
+            catch
+            {
+                if (MessageBox.Show("What a foolish mistake you've just made lol. You're supposed to open AustinEngine.exe, not this, goofy ahh :skull: :skull: :skull:", "Foolish mistake") == DialogResult.OK) Application.Exit();
+            }
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void RestartBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(@"AustinEngine.exe");
+                Application.Exit();
+            }
+            catch
+            {
+                MessageBox.Show("Error: Unable to find \"AustinEngine.exe\". You changed the engine's filename, didn't you..","Error");
+            }
+        }
+    }
+}
